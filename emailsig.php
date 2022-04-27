730e99d4e46651254951ab312d03b647207a9c98
kandroni <104534476+kandroni@users.noreply.github.com>
2022-04-27 15:58:27 -0700

    Add files via upload

---
+++ emailsig.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Email Sig PHP</title>
+</head>
+<body>
+	&nbsp;<br>
+<table border="0" cellpadding="0" cellspacing="0" width="250" style="orphans: 2; widows: 2; table-layout: fixed;"><tbody>
+<tr><td align="left" valign="top" width="57"><a href="http://ways-means.co/" style="text-decoration: none;">
+<img src="https://static1.squarespace.com/static/61d74b78e47be71decc44247/t/6269c9e9591ebf709d51613f/1651100137720/iwi-logo.pnghttps://static1.squarespace.com/static/61d74b78e47be71decc44247/t/6269c9e9591ebf709d51613f/1651100137720/iwi-logo.png" alt="IWI" width="49" height="49" border="0"></a></td><td align="left" valign="top" nowrap="nowrap" width="NaN">
+<div>
+<p style="font-family: Helvetica, Arial, sans-serif; font-size: 10px; line-height: 13px; color: rgb(33, 33, 33); margin: 0px;"><span style="font-weight: bold; display: inline;"></span>&nbsp;<span style="font-weight: normal; font-style: italic; display: inline;">&nbsp;&nbsp;</span><br><span style="display: inline; font-weight:bold">, IT'S WHAT'S INSIDE </span>&nbsp;<br><a href="http://ways-means.co/" style="color: rgb(33, 33, 33); text-decoration: none; display: inline;">C &nbsp;<span style="font-weight: bold; display: inline;"><a href="tel:-" style="color: rgba(0,0,162,1.00); text-decoration: none; display: inline; font-weight:bold">610 937-9995</a></p></div></td></tr></tbody></table>
+
+</body>
+</html>
